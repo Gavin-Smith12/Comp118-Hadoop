a3b3b2345e0fb3fa887114cb247cfaea21ece8c5
Malcolm <malcolm.akinje@gmail.com>
2020-04-14 14:53:45 -0400

    Reducer 2 started.

---
+++ reducer_2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import ast
+
+# Modern_art [20160601,20160609] [50,75] input
+# Modern_art\t[20160609,20160601]\t[75,50]\t125\t25 output
+
+for line in sys.stdin:
+    dates_to_views = {}
+
+    words = line.split()
+    article = words[0]
+    dates = ast.literal_eval(words[1])
+    views = ast.literal_eval(words[2])
+    
+    # maps dates to number of views
+    for i in range(0,len(dates)):
+        dates_to_views[int(dates[i])] = int(views[i])
+        dates[i] = int(dates[i])
+    
+    # dates is sorted
+    dates.sort()
+
+    first_half = 0
+    second_half = 0
+
+    # views is ordered
+    ordered_views = []
+    for i in range(0, len(dates)):
+        ordered_views.append(dates_to_views[dates[i]])
+        if dates[i] < 20160608:
+            first_half += dates_to_views[dates[i]]
+        else:
+            second_half += dates_to_views[dates[i]]
+
+    print(f"{article}\t{str(dates)}\t{str(ordered_views)}\t{sum(ordered_views)}\t{second_half - first_half}")
